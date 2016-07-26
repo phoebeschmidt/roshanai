c75dd050e2015d9b999acc454eb6e214863af1fe
oberkowitz <ojberkowitz@gmail.com>
2016-07-25 23:29:14 -0700

    First draft of midi message handling

--- testApp/roshanai_serial.py
+++ testApp/roshanai_serial.py
@@ -28,68 +28,70 @@ def print_device_info():
     _print_device_info()
     pygame.midi.quit()
 
-def _print_device_info():
-    for i in range( pygame.midi.get_count() ):
-        r = pygame.midi.get_device_info(i)
-        (interf, name, input, output, opened) = r
+class RealTimeMidiThread(Thread):
+    def __init__(self, ser, input_id):
+        Thread.__init__(self)
+        self.serial = ser
+        self.input_id = input_id
 
-        in_out = ""
-        if input:
-            in_out = "(input)"
-        if output:
-            in_out = "(output)"
+    def run(self):
+        pygame.init()
+        pygame.fastevent.init()
+        pygame.midi.init()
 
-        print ("%2i: interface :%s:, name :%s:, opened :%s:  %s" %
-               (i, interf, name, opened, in_out))
+        _print_device_info()
 
-def input_main(device_id = None):
-    pygame.init()
-    pygame.fastevent.init()
-    event_get = pygame.fastevent.get
-    event_post = pygame.fastevent.post
+        if self.input_id is None:
+            input_id = pygame.midi.get_default_input_id()
+        else:
+            input_id = self.input_id
 
-    pygame.midi.init()
+        print ("using input_id :%s:" % input_id)
+        i = pygame.midi.Input(input_id)
 
-    _print_device_info()
+        pygame.display.set_mode((1,1))
 
+        global running
+        while True:
+            if (not running):
+                break
 
-    if device_id is None:
-        input_id = pygame.midi.get_default_input_id()
-    else:
-        input_id = device_id
+            pygame.fastevent.pump() # this needs to be done in order for pygame to not freeze up
+            if i.poll():
+                midi_events = i.read(10)
+                # convert them into pygame events.
+                midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
 
-    print ("using input_id :%s:" % input_id)
-    i = pygame.midi.Input( input_id )
+                for m_e in midi_evs:
+                    print m_e
+                    note = m_e.data1
+                    velocity = m_e.data2
+                    if m_e.data2 == 0:
+                        onOff = 0
+                    else:
+                        onOff = 1
+                    board_id = int(note) / 8 # python defaults to floor division for ints
+                    relay_id = int(note) % 8
+                    print "sending signal: board:%d relay:%d onOff:%d" % (board_id, relay_id, onOff)
+                    self.sendSignal(board_id, note % 8, onOff)
+        del i
+        pygame.midi.quit()
 
-    pygame.display.set_mode((1,1))
-
-
-
-    going = True
-    while going:
-        events = event_get()
-        for e in events:
-            if e.type in [QUIT]:
-                going = False
-            if e.type in [KEYDOWN]:
-                going = False
-            if e.type in [pygame.midi.MIDIIN]:
-                print (e)
-
-        if i.poll():
-            midi_events = i.read(10)
-            # convert them into pygame events.
-            midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
-
-            for m_e in midi_evs:
-                event_post( m_e )
-
-    del i
-    pygame.midi.quit()
+    def sendSignal(self, board_id, relay_id, onOff):
+        '''
+        controller_id - int
+        number - int 0-7 for the specific relay to fire
+        onOff - 0 or 1
+        '''
+        sendString = "!%02d" % board_id
+        dataString = "%i%s" % (relay_id, onOff)
+        sendString = sendString + dataString + "." # "." means end of command. "~" means you can send multiple
+        print sendString
+        self.serial.write(sendString.encode())
 
 
 class SignalSendingThread(Thread):
-    def __init__(self, ser , requestQueue):
+    def __init__(self, ser, requestQueue):
         ''' Constructor. '''
         Thread.__init__(self)
         self.serial = ser
@@ -126,7 +128,7 @@ class SignalSendingThread(Thread):
 
 
     def sendSignal(self, controller_id, signalArray):
-        ''' Send timesliced signals to the serial port, one for each poofer '''
+        ''' Send timesliced signals to the serial port, one for each whacker '''
         sendString = "!0%s" % controller_id
         for i in range(0, len(signalArray)):
             onOff = signalArray[i]
@@ -165,7 +167,7 @@ class SignalSendingThread(Thread):
 
 
 
-class PooferTesterHttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class WhackerTesterHttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path.endswith(".png"):
                 f=open("." + self.path, "rb")
@@ -215,8 +217,6 @@ class PooferTesterHttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             return
 
     def do_POST(self):
-        global requestQueue
-
         ctype, pdict = parse_header(self.headers['content-type'])
         if ctype == 'multipart/form-data':
             postvars = parse_multipart(self.rfile, pdict)
@@ -265,19 +265,60 @@ def initSerial():
     ser.open()
     return ser
 
+def print_device_info():
+    pygame.midi.init()
+    _print_device_info()
+    pygame.midi.quit()
+
+def _print_device_info():
+    for i in range( pygame.midi.get_count() ):
+        r = pygame.midi.get_device_info(i)
+        (interf, name, input_val, output_val, opened) = r
+
+        in_out = ""
+        if input_val:
+            in_out = "(input)"
+        if output_val:
+            in_out = "(output)"
+
+        print ("%2i: interface :%s:, name :%s:, opened :%s:  %s" %
+               (i, interf, name, opened, in_out))
+
+def usage():
+    print ("--real_time_midi [device_id] : Real time input midi mode")
+    print ("--pattern : Web based pattern player")
+    print ("--list : list available midi devices")
 
 if __name__ == '__main__':
     running = True
     PORT = 8666
-
-    ser = initSerial()
-    signalQueue = Queue.Queue()
-    signalProcessingThread = SignalSendingThread(ser, signalQueue)
-    signalProcessingThread.start()
-    httpd = BaseHTTPServer.HTTPServer(("localhost", PORT), PooferTesterHttpHandler, signalQueue)
     try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print "Keyboard interrupt detected, terminating"
-        running = False
-    httpd.server_close()
+        device_id = int( sys.argv[-1] )
+    except:
+        device_id = None
+    ser = initSerial()
+    if "--real_time_midi" in sys.argv or "-m" in sys.argv:
+
+        real_time_midi_thread = RealTimeMidiThread(ser, device_id)
+        real_time_midi_thread.start()
+        try:
+            while True:
+                continue
+        except KeyboardInterrupt:
+            print "Keyboard interrupt detected, terminating"
+            running = False
+    elif "--list" in sys.argv or "-l" in sys.argv:
+        print_device_info()
+    elif "--pattern" in sys.argv or "-p" in sys.argv:
+        signalQueue = Queue.Queue()
+        signalProcessingThread = SignalSendingThread(ser, signalQueue)
+        signalProcessingThread.start()
+        httpd = BaseHTTPServer.HTTPServer(("localhost", PORT), WhackerTesterHttpHandler, signalQueue)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print "Keyboard interrupt detected, terminating"
+            running = False
+        httpd.server_close()
+    else:
+        usage()
