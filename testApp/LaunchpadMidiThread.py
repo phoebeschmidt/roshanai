643a9cd22addf8a04956fda8f68a7c3eea2d728b
oberkowitz <ojberkowitz@gmail.com>
2016-07-26 00:37:30 -0700

    Adding a map so we can figure out what note we are playing when we fire a particular relay and board id

--- testApp/LaunchpadMidiThread.py
+++ testApp/LaunchpadMidiThread.py
@@ -2,16 +2,21 @@ from roshanai_serial import RealTimeMidiThread
 
 class LaunchpadMidiThread(RealTimeMidiThread):
     def __init__(self, ser, input_id):
-        self.noteMap = {}
+        self.noteToSolenoidMap = {}
+        self.relayAndBoardToNoteMap = {}
         super(LaunchpadMidiThread, self).__init__(ser, input_id)
 
     def noteToBoardId(self, note):
-        mapped_board = self.noteMap[note][0]
+        mapped_board = self.noteToSolenoidMap[note][0]
         return mapped_board
 
     def noteToRelayId(self, note):
-        mapped_relay = self.noteMap[note][1]
+        mapped_relay = self.noteToSolenoidMap[note][1]
         return int(note) % 8
 
     def createMapping(self, note, board_id, relay_id):
-        self.noteMap[note] = (board_id, relay_id)
+        self.noteToSolenoidMap[note] = (board_id, relay_id)
+        self.relayAndBoardToNoteMap[(board_id, relay_id)] = note
+
+    def relayAndBoardToNote(self, board_id, relay_id):
+        return self.relayAndBoardToNoteMap[(board_id, relay_id)]
