57673f4f70d753bbf4de96aed607404acaf4b6c9
oberkowitz <ojberkowitz@gmail.com>
2016-08-01 23:36:37 -0700

    Finished the midi functionality with rtmidi. We now have the ability to start a thread with multiple programs, and we have defaults for when a mapping isn't present in a program. Next, we need to add solenoid_id to relay and board id mappings.

--- testApp/roshanai_serial.py
+++ testApp/roshanai_serial.py
@@ -34,10 +34,12 @@ available_apis = get_compiled_api()
 
 
 class RealTimeMidiThread(Thread):
-    def __init__(self, ser, input_id):
+    def __init__(self, ser, input_id, programs):
         Thread.__init__(self)
         self.serial = ser
         self.input_id = input_id
+        self.programs = programs
+        self.current_program = programs[0]
 
     def run(self):
         timer = time.time()
@@ -54,6 +56,7 @@ class RealTimeMidiThread(Thread):
         global running
         while True:
             if (not running):
+                print "Thread finished running"
                 break
             while True:
                 msg = midi_in.get_message()
@@ -61,37 +64,47 @@ class RealTimeMidiThread(Thread):
                 if msg:
                     message, deltatime = msg
                     timer += deltatime
+                    status = message[0] & 0xf0 # bit mask for the status message so we ignore channel number
+                    if status == 0x90: # Note on
+                        note = message[1]
+                        velocity = message[2]
+                        if velocity == 0:
+                            self.sendSignal(note, 0) # note on with velocity 0 is a note off
+                        else:
+                            self.sendSignal(note, 1) # note on
+                    if status == 0x80: # Note off
+                        note = message[1]
+                        self.sendSignal(note, 0)
+                    if status == 0xc0: # Program change
+                        program = message[1]
+                        self.change_program(program)
                     print("[%s] @%0.6f %r" % (port_name, timer, message))
 
-                time.sleep(0.01)
-        # Need to include logic based on midi note values
-                #     note = m_e.data1
-                #     velocity = m_e.data2
-                #     if m_e.data2 == 0:
-                #         onOff = 0
-                #     else:
-                #         onOff = 1
-                #     board_id = noteToBoardId(note)
-                #     relay_id = noteToRelayId(note)
-                #     print "sending signal: board:%d relay:%d onOff:%d" % (board_id, relay_id, onOff)
-                #     self.sendSignal(board_id, note % 8, onOff)
-    def noteToBoardId(self, note):
-        return int(note) / 8 # python defaults to floor division for ints. Every 8 notes up we move to another board
-    def noteToRelayId(self, note):
-        return int(note) % 8
+    def change_program(self, program_index):
+        self.current_program = self.programs[program_index]
 
-    def sendSignal(self, board_id, relay_id, onOff):
+    def sendSignal(self, midi_note, onOff):
         '''
         controller_id - int
-        number - int 0-7 for the specific relay to fire
+        midi_note -
         onOff - 0 or 1
         '''
+        try:
+            board_id, relay_id = self.current_program[midi_note]
+        except:
+            board_id = (int(midi_note) / 8) + 1 # python defaults to floor division for ints. Every 8 notes up we move to another board
+            relay_id = (int(midi_note) % 8) + 1
+
         sendString = "!%02d" % board_id
         dataString = "%i%s" % (relay_id, onOff)
         sendString = sendString + dataString + "." # "." means end of command. "~" means you can send multiple
         print sendString
         self.serial.write(sendString.encode())
 
+    def stopSignals(self):
+        ''' Stop sending signals, send 'all off' command to terminate pattern'''
+        sendString = "!0000."
+        self.serial.write(sendString.encode())
 
 class SignalSendingThread(Thread):
     def __init__(self, ser, requestQueue):
@@ -308,11 +321,10 @@ if __name__ == '__main__':
         device_id = int( sys.argv[-1] )
     except:
         device_id = None
-    # ser = initSerial()
-    ser = None
+    ser = initSerial()
     if "--real_time_midi" in sys.argv or "-m" in sys.argv:
-
-        real_time_midi_thread = RealTimeMidiThread(ser, device_id)
+        test_programs = [{0:(0,0)}] # this is only for testing now, we need to create real programs
+        real_time_midi_thread = RealTimeMidiThread(ser, device_id, test_programs)
         real_time_midi_thread.start()
         try:
             while True:
